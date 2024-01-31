59d84f9b309b1126c422d3ad3d86af8beae5d40e
mrsev17 <mr.upgrade3@protonmail.com>
2024-02-01 00:27:18 +0200

    Added styles for tablet and mobile view in workit-landing-page

--- workit-landing-page/src/components/Footer/Footer.js
+++ workit-landing-page/src/components/Footer/Footer.js
@@ -1,17 +1,9 @@
-import logoFacebook from "../../assets/icon-facebook.svg";
-import logoTwitter from "../../assets/icon-twitter.svg";
-import logoInstagram from "../../assets/icon-instagram.svg";
+import { linksData } from "../../utils/data";
 import logoDark from "../../assets/logo-dark.svg";
 import styles from "./Footer.module.css";
 import "../../globalStyles.css";
 
 export const Footer = () => {
-    const linksData = [
-        { icon: logoFacebook, iconName: "Facebook" },
-        { icon: logoTwitter, iconName: "Twitter" },
-        { icon: logoInstagram, iconName: "Instagram" },
-    ];
-
     return (
         <footer>
             <div className="container d-flex flex-col flex-center flex-aic gap-footer">
@@ -28,22 +20,12 @@ export const Footer = () => {
                                     <img
                                         className={styles.networkIcon}
                                         src={item.icon}
-                                        alt={`Picture of network logo ${item.iconName}`}
+                                        alt={`Link of network ${item.iconName}`}
                                     />
                                 </a>
                             </li>
                         );
                     })}
-                    {/* <li>
-                        <a href="#">
-                            <img
-                                src={logoDark}
-                                alt="Picture of Logo in dark color"
-                            />
-                        </a>
-                    </li>
-                    <li></li>
-                    <li></li> */}
                 </ul>
             </div>
         </footer>
---
+++ workit-landing-page/src/utils/data.js
@@ -0,0 +1,27 @@
+import logoFacebook from "../assets/icon-facebook.svg";
+import logoTwitter from "../assets/icon-twitter.svg";
+import logoInstagram from "../assets/icon-instagram.svg";
+
+export const advantagesData = [
+    {
+        order: "1",
+        title: "Actionable insights",
+        text: " Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics.",
+    },
+    {
+        order: "2",
+        title: "Data-driven decisions",
+        text: "Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.",
+    },
+    {
+        order: "3",
+        title: "Always affordable",
+        text: "Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.",
+    },
+];
+
+export const linksData = [
+    { icon: logoFacebook, iconName: "Facebook" },
+    { icon: logoTwitter, iconName: "Twitter" },
+    { icon: logoInstagram, iconName: "Instagram" },
+];
