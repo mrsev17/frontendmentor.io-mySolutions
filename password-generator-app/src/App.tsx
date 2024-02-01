9f110064870d0f2049526bc50e8ad8dc5a716688
mrsev17 <mr.upgrade3@protonmail.com>
2024-02-01 16:25:52 +0200

    Init project password-generator-app

---
+++ password-generator-app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Test Font Family</h1>
+            <h2>Check</h2>
+        </div>
+    );
+}
+
+export default App;
